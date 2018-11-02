849b05ead040f8cbc4254c3c2751dff39c13fdbf
len0rd <tylerm15@gmail.com>
2018-11-02 13:36:39 -0600

    add catkin stuff, controller_ros class

--- include/multirotor_sim/controller_ros.h
+++ include/multirotor_sim/controller_ros.h
@@ -3,3 +3,16 @@
 #include <ros/ros.h>
 #include <ros/package.h>
 #include <nav_msgs/Odometry.h>
+
+class Controller_ROS
+{
+public:
+    Controller_ROS();
+    void odometry_callback(const nav_msgs::Odometry& msg);
+
+private: 
+    ros::NodeHandle nh_;
+    ros::Subscriber odometry_sub_;
+    ros::Publisher command_pub_;
+    controller::Controller controller_;
+};
--- src/controller_ros.cpp
+++ src/controller_ros.cpp
@@ -1,21 +1,25 @@
 #include <controller_ros.h>
 
 
-
-
-void odometry_callback(const nav_msgs::Odometry& msg){
-
+Controller_ROS::Controller_ROS() :
+    nh_("~")
+{
+    controller_ = controller::Controller();
+    controller.load("~/catkin_ws/src/multirotor_sim/params/sim_params.yaml"); //todo: change to ros params?
+    odometry_sub_ = nh.subscribe("nav_msgs/Odometry", 100, &odometry_callback, this);
+    command_pub_  = nh.advertise<rosflight_io::Command>("command", 100);
 }
 
+Controller_ROS::odometry_callback(const nav_msgs::Odometry& msg)
+{
+    std::cout << "odom callback" << endl;
+}
 
 int main(int argc, char* argv[])
 {
     ros::init(argc, argv, "multirotor_controller_node");
 
-    ros::NodeHandle nh;
-
-    ros::Subscriber odometry_sub_ = nh.subscribe("nav_msgs/Odometry", 1, &odometry_callback, this);
-    ros::Publisher command_pub_ = nh.advertise<rosflight_io::Command>()
+    Controller_ROS controller_ros;
 
     ros::spin();
 
