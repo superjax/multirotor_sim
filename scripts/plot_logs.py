aa15a0710f774fccad84808eb4fcd362eee4726c
len0rd <tylerm15@gmail.com>
2018-11-14 17:06:04 -0700

    basic plots

--- scripts/plot_logs.py
+++ scripts/plot_logs.py
@@ -2,8 +2,7 @@
 
 import os
 import numpy as np
-# from tqdm import tqdm # progress bar
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def main():
     ODOM_VAL_PER_ROW = 14
@@ -19,8 +18,26 @@ def main():
     rawCommand = np.fromfile(logDir + 'command.bin', float, -1, "")
     rawCommand = np.reshape(rawCommand, (-1, COMMAND_VAL_PER_ROW))
 
-    # Formatted Time, Thrust, X, Y, Z for clmns
-    print(rawCommand[0:4,:])
+    # Command column format: time, thrust, x, y, z
+    commandYLabels = ['Command: Thrust', 'Command: X', 'Command: Y', 'Command: Z']
+    colors  = ['b-', 'r-', 'y-', 'g-']
+
+    plt.figure('Command Out')
+    for i in range(1,COMMAND_VAL_PER_ROW):
+        plt.subplot(2,2,i)
+        plt.plot(rawCommand[:,0], rawCommand[:,i], colors[i-1])
+        plt.ylabel(commandYLabels[i-1])
+
+
+    odomPoseLabels = ['Estimator: X', 'Estimator: Y', 'Estimator: Z']
+    plt.figure("Estimator Pose In")
+    for i in range(1, 4):
+        plt.subplot(3,1,i)
+        plt.plot(rawOdom[:,0], rawOdom[:,i], colors[i-1])
+        plt.ylabel(odomPoseLabels[i-1])
+
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
